2883e8ec6b1e7795126e46982f2ff9395263548b
jakejonggubaek <jakejonggubaek@gmail.com>
2021-03-18 16:02:51 -0400

    dark-mode-theme

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import logo from './assets/logo.png';
 
 function Header() {
+
+  const [theme, setTheme] = useState(localStorage.getItem("dark-mode"));
+
+  const body = document.querySelector('body');
+
+  let darkMode = localStorage.getItem("dark-mode");
+
+  if (darkMode === "dark") {
+    body.classList.add('dark-theme-body');
+  }
+
+  const handleClick = () => {
+    if (darkMode === "dark") {
+      body.classList.remove('dark-theme-body');
+      localStorage.setItem("dark-mode", "light");
+      darkMode = 'light';
+      setTheme('light');
+
+    } else {
+      body.classList.add('dark-theme-body');
+      localStorage.setItem("dark-mode", "dark");
+      darkMode = 'dark';
+      setTheme('dark');
+    }
+  }
+
   return (
     <header className="wrapper">
       <h2><img className="logo" src={logo} alt="popsicle logo" /> Popsipoll</h2>
@@ -9,6 +36,15 @@ function Header() {
         <ul>
           <li><Link to={`/pollbooths`}>Poll Booths</Link></li>
           <li><Link to="/">Create Poll</Link></li>
+          <li>
+            <button className="theme-button" onClick={handleClick}>
+            {
+              theme === 'dark' ?
+                "Light Mode"
+                : "Dark Mode"
+            }
+            </button>
+          </li>
         </ul>
       </nav>
     </header>
